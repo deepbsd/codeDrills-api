3c29134403b56114a8be6b22bdf758c2412ed3e1
David S. Jackson <deepbsd@yahoo.com>
2018-01-21 14:47:03 -0500

    just getting the cors going and starting to pass a simple endpoint test

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
-app.get('/api/*', (req, res) => {
-  res.json({ok: true});
-});
+// const cors = require('cors');
+// const {CLIENT_ORIGIN} = require('./config');
+//
+// app.use(
+//     cors({
+//         origin: CLIENT_ORIGIN
+//     })
+// );
+const cors = require('cors');
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.use(cors());
+
+// app.get('/api/*', (req, res) => {
+//   res.json({ok: true});
+// });
+
+app.get('/api/*', function (req, res, next) => {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+// app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+app.listen(PORT,  () => console.log(`CORS-enabled web server listening on port ${PORT}`));
 
 module.exports = {app};
