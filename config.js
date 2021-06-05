4efc53d86ae092f49fb565f6cb0f8e2f012f4525
David S. Jackson <deepbsd@yahoo.com>
2021-06-05 13:25:19 -0400

    adding back rewriteTries and w variables

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                       //'mongodb://deepbsd:2D33p4m3!@ds115758.mlab.com:15758/codedrills';
                       //'mongodb://deepbsd:WRSt7MVDsSTN!fM@codedrills.zm5qb.mongodb.net/codeDrills?retryWrites=true&w=majority';
                       //'mongodb+srv://deepbsd:WRSt7MVDsSTN!fM@codedrills.zm5qb.mongodb.net/codeDrills?retryWrites=true&w=majority';
-                      'mongodb+srv://deepbsd:25EAz7msb+D5UJ#@codedrills.zm5qb.mongodb.net/codeDrills';
+                      'mongodb+srv://deepbsd:25EAz7msb+D5UJ#@codedrills.zm5qb.mongodb.net/codeDrills?retryWrites=true&w=majority';
 exports.TEST_DATABASE_URL = (
    process.env.TEST_DATABASE_URL ||
    //'mongodb://localhost/testapi');
