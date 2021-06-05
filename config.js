7276d9b5085659cad9e688b054bffa0a304c1143
David S. Jackson <deepbsd@yahoo.com>
2021-06-05 13:37:33 -0400

    deleting an old comment

--- config.js
+++ config.js
@@ -2,7 +2,6 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                       //'mongodb://localhost/codeDrills';
                       //'mongodb://deepbsd:2D33p4m3!@ds115758.mlab.com:15758/codedrills';
-                      //'mongodb://deepbsd:WRSt7MVDsSTN!fM@codedrills.zm5qb.mongodb.net/codeDrills?retryWrites=true&w=majority';
                       //'mongodb+srv://deepbsd:WRSt7MVDsSTN!fM@codedrills.zm5qb.mongodb.net/codeDrills?retryWrites=true&w=majority';
                       'mongodb+srv://deepbsd:25EAz7msb+D5UJ#@codedrills.zm5qb.mongodb.net/codeDrills?retryWrites=true&w=majority';
 exports.TEST_DATABASE_URL = (
