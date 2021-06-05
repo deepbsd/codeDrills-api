c7659a7cfb86b4d3a0ddeef3c60e880b328b05e1
David S. Jackson <deepbsd@yahoo.com>
2021-06-05 00:09:16 -0400

    reconnectTries was incompatable with new version of mongoose

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ const options = {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   autoIndex: false, // Don't build indexes
-  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+  //reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
   //reconnectInterval: 500, // Reconnect every 500ms
   poolSize: 10, // Maintain up to 10 socket connections
   // If not connected, return errors immediately rather than waiting for reconnect
